1fea14acfabd73ecddb362b931e71895697e0be2
Chandler <cjoynes@umich.edu>
2023-11-30 20:14:49 -0500

    Changed positive and negative code

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -18,18 +18,18 @@ class testSelectionSort {
 	
 	public void testPositive(){
 		int[] arr = new int[5];
-		arr[0] = 8;
-		arr[1] = 9;
-		arr[2] = 7;
-		arr[3] = 10;
-		arr[4] = 2;
+		arr[0] = 3;
+		arr[1] = 5;
+		arr[2] = 2;
+		arr[3] = 8;
+		arr[4] = 6;
 		
 		int[] Sortedarr = new int[5];
 		Sortedarr[0] = 2;
-		Sortedarr[1] = 7;
-		Sortedarr[2] = 8;
-		Sortedarr[3] = 9;
-		Sortedarr[4] = 10;
+		Sortedarr[1] = 3;
+		Sortedarr[2] = 5;
+		Sortedarr[3] = 6;
+		Sortedarr[4] = 8;
 		
 		int[] actualSortarr = array.basicSelectionSort(arr);
 		
@@ -54,18 +54,18 @@ class testSelectionSort {
 	
 	public void testNegative(){
 		int[] arr = new int[5];
-		arr[0] = -2;
-		arr[1] = -7;
-		arr[2] = -3;
-		arr[3] = -9;
-		arr[4] = -4;
+		arr[0] = -6;
+		arr[1] = -3;
+		arr[2] = -8;
+		arr[3] = -1;
+		arr[4] = -7;
 		
 		int[] Sortedarr = new int[5];
-		Sortedarr[0] = -9;
+		Sortedarr[0] = -8;
 		Sortedarr[1] = -7;
-		Sortedarr[2] = -4;
+		Sortedarr[2] = -6;
 		Sortedarr[3] = -3;
-		Sortedarr[4] = -2;
+		Sortedarr[4] = -1;
 		
 		int[] actualSortarr = array.basicSelectionSort(arr);
 		
